12a069640489417638a5aa02af6cacd20e4dbb87
Alessandro Power <alessandrojpower@gmail.com>
2017-07-16 09:37:36 -0400

    Added Procfile for Heroku deployment.

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,9 @@
 """Gunicorn configuration file."""
+import os
 
-bind = ['0.0.0.0:5000']
+PORT = os.environ.get("PORT", 5000)
+
+bind = ['0.0.0.0:{port}'.format(port=PORT)]
 workers = 2
 worker_class = 'geventwebsocket.gunicorn.workers.GeventWebSocketWorker'
 proc_name = 'HAB-MC'
